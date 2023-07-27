26e0f251815801232281f61eb1d73f360fbfc403
Pablo Leite <pabloleite03@gmail.com>
2023-07-27 15:03:00 -0300

    Upgrade the project. Include Firebase Firestore.

--- app/src/main/java/app/kotlin/devpbleite/barbershop/adapter/ServicosAdapter.kt
+++ app/src/main/java/app/kotlin/devpbleite/barbershop/adapter/ServicosAdapter.kt
@@ -1,4 +1,33 @@
 package app.kotlin.devpbleite.barbershop.adapter
 
-class ServicosAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import app.kotlin.devpbleite.barbershop.databinding.ServicosItemBinding
+import app.kotlin.devpbleite.barbershop.model.Servicos
+
+class ServicosAdapter(private val context: Context, private val listaServicos: MutableList<Servicos>):
+  RecyclerView.Adapter<ServicosAdapter.ServicosViewHolder>() {
+    
+  
+  override fun onCreateViewHolder(
+    parent: ViewGroup,
+    viewType: Int
+  ): ServicosViewHolder {
+    val itemLista = ServicosItemBinding.inflate(LayoutInflater.from(context), parent, false)
+    return ServicosViewHolder(itemLista)
+  }
+  
+  override fun getItemCount() = listaServicos.size
+  
+  override fun onBindViewHolder(holder: ServicosViewHolder, position: Int) {
+    holder.imgServico.setImageResource(listaServicos[position].img!!)
+    holder.txtServico.text = listaServicos[position].nome
+  }
+  
+  inner class ServicosViewHolder(binding: ServicosItemBinding): RecyclerView.ViewHolder(binding.root) {
+    val imgServico = binding.imgServico
+    val txtServico = binding.txtServico
+  }
 }
--- app/src/main/java/app/kotlin/devpbleite/barbershop/view/Home.kt
+++ app/src/main/java/app/kotlin/devpbleite/barbershop/view/Home.kt
@@ -1,13 +1,19 @@
 package app.kotlin.devpbleite.barbershop.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import app.kotlin.devpbleite.barbershop.R
+import app.kotlin.devpbleite.barbershop.adapter.ServicosAdapter
 import app.kotlin.devpbleite.barbershop.databinding.ActivityHomeBinding
+import app.kotlin.devpbleite.barbershop.model.Servicos
 
 class Home : AppCompatActivity() {
   
   private lateinit var binding: ActivityHomeBinding
+  private lateinit var servicosAdapter: ServicosAdapter
+  private val listaServicos: MutableList<Servicos> = mutableListOf()
   
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -17,7 +23,33 @@ class Home : AppCompatActivity() {
     supportActionBar?.hide()
     val nome = intent.extras?.getString("nome")
     
-    binding.txtNomeUsuario.text = "Bem-vindo(a), $nome!"
+    binding.txtNomeUsuario.text = "Bem-vindo, $nome!"
+    val recyclerViewServicos = binding.recyclerViewServicos
+    recyclerViewServicos.layoutManager = GridLayoutManager(this, 2)
+    servicosAdapter = ServicosAdapter(this, listaServicos)
+    recyclerViewServicos.setHasFixedSize(true)
+    recyclerViewServicos.adapter = servicosAdapter
+    getServicos()
     
+    binding.btAgendar.setOnClickListener {
+      val intent = Intent(this, Agendamento::class.java)
+      intent.putExtra("nome", nome)
+      startActivity(intent)
+    }
+    
+  }
+  
+  private fun getServicos() {
+    val servico1 = Servicos(R.drawable.img1, "Corte de Cabelo")
+    listaServicos.add(servico1)
+  
+    val servico2 = Servicos(R.drawable.img2, "Barba")
+    listaServicos.add(servico2)
+  
+    val servico3 = Servicos(R.drawable.img3, "Lavagem de Cabelo")
+    listaServicos.add(servico3)
+  
+    val servico4 = Servicos(R.drawable.img4, "Tratamentos para o Cabelo")
+    listaServicos.add(servico4)
   }
 }
